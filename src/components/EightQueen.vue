158799ed002b67114fe37a58334b314ad31eaaf5
Grace Wu <1745282879@qq.com>
2021-12-07 00:12:15 +0800

    feat:补充样式

--- src/components/EightQueen.vue
+++ src/components/EightQueen.vue
@@ -4,7 +4,12 @@
 
     <div class="grid">
       <div class="row" v-for="(row, rowIndex) in grids" :key="rowIndex">
-        <div class="cell" v-for="cell in row" :key="cell.key">
+        <div
+          class="cell"
+          v-for="(cell, cIndex) in row"
+          :key="cell.key"
+          @click.stop="select(rowIndex, cIndex)"
+        >
           <div v-if="cell.ok">Q</div>
         </div>
       </div>
@@ -29,8 +34,40 @@ export default {
       grids,
     };
   },
+  methods: {
+    select(rowIndex, cIndex) {
+      this.grids[rowIndex][cIndex].ok = true;
+    },
+  },
 };
 </script>
 
-<style lang="less" scoped>
+<style scoped>
+.grid {
+  width: 400px;
+  margin: 0 auto;
+}
+.row {
+  width: 400px;
+  height: 50px;
+  display: flow-root;
+}
+.row:nth-child(2n) .cell:nth-child(2n) {
+  background: #999;
+}
+.row:nth-child(2n) .cell:nth-child(2n - 1) {
+  background: #efefef;
+}
+.cell {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  background: #999;
+  float: left;
+  cursor: pointer;
+}
+.cell:nth-child(2n) {
+  background: #efefef;
+}
 </style>
